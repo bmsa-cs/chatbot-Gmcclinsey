c010c1400fc4395adf7c4b24408cdf2b5012ba6d
Gmcclinsey <73853145+Gmcclinsey@users.noreply.github.com>
2021-01-22 13:16:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,64 @@ if __name__ == "__main__":
   t = input("Run pytest? (y/n)").lower()
   if t == 'y':
     run_tests()
+    
+    
+    
+    
+    
+    x=input("Hi whats your name")
+print(x + " That's a cool name")
+
+
+n=input("whats your favorite sport")
+b=0
+# line 8-29 are all  about the sport question
+if((n =="basketball" )):
+  print("I like basketball too")
+
+if((n =="baseball" )):
+  print("Baseball is pretty cool but I like lacrosse a little more")
+
+if((n =="soccer" )):
+  print("I really like soccer I even played it for a while")
+
+if((n =="motorcross" )):
+  print("Motorcross is my favorite too")
+
+if((n =="tennis" )):
+  b=input("I have never tried tennis is it fun")
+if((b == "yes")):
+  print("cool maybe I'll try it sometime")
+
+if((b == "yeah")):
+  print("cool maybe I'll try it sometime")
+
+if((n == "football")):
+  print(" Football looks pretty fun")
+
+t=input(" How was your day so far today")
+
+if((t == "good" or  "great")):
+  print(" Thats awesome")
+else:
+    print("well Im sorry about that")
+
+
+
+#this is where the random number generator but now im stuck
+
+
+answer=input(" Guess a number 1-10")
+import random
+number=random.randint(1,10)
+while answer != number:
+  if answer >= number:
+    print ("Too high!")
+  else:
+    
+    print ("Too low!")
+  answer = int(input("Try again!"))
+ 
+print ("Congratulations!")
+
+    
